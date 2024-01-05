9ddf62e253c6efb80bd8123d1305909a714e127a
zaeso <140312201+zaeso@users.noreply.github.com>
2024-01-05 14:03:10 +0300

    Update main.py

--- portfolio-main/main.py
+++ portfolio-main/main.py
@@ -4,7 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
 db = SQLAlchemy(app)
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get("button_html")
+    button_db=request.form.get("button_db")
 
+    return render_template('index.html', button_python=button_python , button_discord=button_discord  , button_html=button_html , button_db=button_db )
 class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False)
@@ -26,15 +33,7 @@ def index():
     return render_template('index.html')
 
 
-#Динамичные скиллы
-@app.route('/', methods=['POST'])
-def process_form():
-    button_python = request.form.get('button_python')
-    button_discord = request.form.get('button_discord')
-    button_html = request.form.get("button_html")
-    button_db=request.form.get("button_db")
 
-    return render_template('index.html', button_python=button_python , button_discord=button_discord  , button_html=button_html , button_db=button_db )
 
     
 
