995a2f0a36153e54e4a2cb086fc0ae695fabdd41
zaeso <140312201+zaeso@users.noreply.github.com>
2024-01-05 21:36:57 +0300

    Update main.py

--- portfolio-main/main.py
+++ portfolio-main/main.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template,request, redirect
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
 db = SQLAlchemy(app)
 
 @app.route('/', methods=['POST'])
