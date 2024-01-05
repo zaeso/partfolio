f2df817b6d3be40d55af6e31ae64c64d0f39d253
zaeso <140312201+zaeso@users.noreply.github.com>
2024-01-05 14:03:49 +0300

    Update main.py

--- portfolio-main/main.py
+++ portfolio-main/main.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feedback.db'
 db = SQLAlchemy(app)
+
 @app.route('/', methods=['POST'])
 def process_form():
     button_python = request.form.get('button_python')
@@ -12,6 +13,8 @@ def process_form():
     button_db=request.form.get("button_db")
 
     return render_template('index.html', button_python=button_python , button_discord=button_discord  , button_html=button_html , button_db=button_db )
+
+
 class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False)
@@ -42,3 +45,6 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
